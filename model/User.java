2323ae896f1cd2fe4e5ce578342dfbc3392d41dd
Ahmedfahd926 <ahmedabdelaziz618@gmail.com>
2026-04-23 22:50:05 +0200

    Add files via upload

---
+++ model/User.java
@@ -0,0 +1,18 @@
+public class User{
+private String name;
+private int number;
+private String email;
+private String address;
+private String username;
+private String password;
+public 
+
+
+
+
+
+
+
+
+
+}
